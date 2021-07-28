d979ade05b4144c926ed051306de039371eb1c34
jeehyuni18 <jeehyuni18@gmail.com>
2021-07-28 14:16:52 +0900

    vBoard에 timestamped 상속

--- src/main/java/com/vaccinelife/vaccinelifeapi/model/VBoard.java
+++ src/main/java/com/vaccinelife/vaccinelifeapi/model/VBoard.java
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-public class VBoard {
+public class VBoard extends Timestamped{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
