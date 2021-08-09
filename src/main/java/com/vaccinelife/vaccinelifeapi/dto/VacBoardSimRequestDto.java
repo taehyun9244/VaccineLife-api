6d7b0b889b60b469a37a71acc5099e5139a3a195
fitChan <84306157+fitChan@users.noreply.github.com>
2021-08-09 17:17:50 +0900

    vacBoardSimple List is added timestamped

--- src/main/java/com/vaccinelife/vaccinelifeapi/dto/VacBoardSimRequestDto.java
+++ src/main/java/com/vaccinelife/vaccinelifeapi/dto/VacBoardSimRequestDto.java
@@ -1,9 +1,17 @@
 package com.vaccinelife.vaccinelifeapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.vaccinelife.vaccinelifeapi.model.User;
 import com.vaccinelife.vaccinelifeapi.model.VacBoard;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,19 +25,29 @@ public class VacBoardSimRequestDto {
     private int totalVisitors;
     private int commentCount;
     private String type;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "asia/seoul" )
+    @CreatedDate // 최초 생성 시점
+    private LocalDateTime createdAt;
 
-
-    @Builder
-    public VacBoardSimRequestDto(Long id, String title, int likeCount, int totalVisitors, int commentCount, String type) {
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss" , timezone = "asia/seoul")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @LastModifiedDate // 마지막 변경 시점
+    private LocalDateTime modifiedAt;
+@Builder
+    public VacBoardSimRequestDto(Long id, String title, int likeCount, int totalVisitors, int commentCount, String type, LocalDateTime createdAt, LocalDateTime modifiedAt) {
         this.id = id;
         this.title = title;
         this.likeCount = likeCount;
         this.totalVisitors = totalVisitors;
-        this.commentCount=commentCount;
+        this.commentCount = commentCount;
         this.type = type;
-
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
     }
-    
+
     public static VacBoardSimRequestDto of(VacBoard vacBoard){
         return VacBoardSimRequestDto.builder()
                 .id(vacBoard.getId())
@@ -38,6 +56,8 @@ public class VacBoardSimRequestDto {
                 .totalVisitors(vacBoard.getTotalVisitors())
                 .commentCount(vacBoard.getCommentCount())
                 .type(vacBoard.getUser().getType())
+                .createdAt(vacBoard.getCreatedAt())
+                .modifiedAt(vacBoard.getModifiedAt())
                 .build();
     }
     
