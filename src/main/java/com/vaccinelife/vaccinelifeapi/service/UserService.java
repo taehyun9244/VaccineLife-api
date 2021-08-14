e6aeb37aff6170cbb841b34c0680102ff5458277
jeehyuni18 <jeehyuni18@gmail.com>
2021-08-14 17:59:02 +0900

    registerUser 기존 정보로 일단 변경

--- src/main/java/com/vaccinelife/vaccinelifeapi/service/UserService.java
+++ src/main/java/com/vaccinelife/vaccinelifeapi/service/UserService.java
@@ -18,6 +18,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @Service
@@ -29,15 +32,12 @@ public class UserService {
     private static final String ADMIN_TOKEN = "AAABnv/xRVklrnYxKZ0aHgTBcXukeZygoC";
 
 
-
     @Autowired
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
     }
-
-
 
 
 
@@ -56,14 +56,26 @@ public class UserService {
         String disease=requestDto.getDisease();
         String afterEffect=requestDto.getAfterEffect();
 
+        Optional<User> found = userRepository.findByUsername(username);
+        Optional<User> nicknameFound = userRepository.findByNickname(nickname);
 
+        Pattern usernamePattern = Pattern.compile("^[a-zA-Z0-9]*$");
+        Matcher usernameMatcher = usernamePattern.matcher(username);
 
         if (username.equals("") || password.equals("") || passwordChecker.equals("")|| nickname.equals("")) {
             throw new IllegalArgumentException("username || password || passwordChecker가 비어있습니다.");
         } else if (password.length() < 8) {
-            throw new IllegalArgumentException("password는 최소 8글자입니다.");
+            throw new IllegalArgumentException("비밀번호는 최소 8글자입니다.");
         } else if (!password.equals(passwordChecker)) {
             throw new IllegalArgumentException("password와 passwordChecker가 다릅니다.");
+        }else if (found.isPresent() && nicknameFound.isPresent()) {
+            throw new IllegalArgumentException("중복된 ID와 닉네임이 존재합니다.");
+        } else if (found.isPresent()) {
+            throw new IllegalArgumentException("중복된 ID가 존재합니다.");
+        }else if (nicknameFound.isPresent()) {
+            throw new IllegalArgumentException("중복된 닉네임이 존재합니다.");
+        }else if(!usernameMatcher.find() || username.length()<6 || username.length()>12){
+            throw new IllegalArgumentException("아이디는 영문 조합하여 6~12자로 구성하세요.");
         }
 
 
@@ -72,6 +84,4 @@ public class UserService {
         User user = new User( id, username, password, role, nickname, isVaccine, type, degree , gender, age, disease, afterEffect);
         userRepository.save(user);
     }
-
-
 }
