2da1142ebd374f6d103f0aed6ad40b4f307451e3
jeehyuni18 <jeehyuni18@gmail.com>
2021-08-19 18:24:52 +0900

    제목 내용 미입력시 message 추가

--- src/main/java/com/vaccinelife/vaccinelifeapi/dto/QuarBoardPostRequestDto.java
+++ src/main/java/com/vaccinelife/vaccinelifeapi/dto/QuarBoardPostRequestDto.java
@@ -5,12 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class QuarBoardPostRequestDto {
     private Long userId;
+    @NotBlank(message = "제목을 입력해주세요.")
     private String title;
+    @NotBlank(message = "내용을 적어주세요")
     private String contents;
 }
--- src/main/java/com/vaccinelife/vaccinelifeapi/dto/VacBoardPostRequestDto.java
+++ src/main/java/com/vaccinelife/vaccinelifeapi/dto/VacBoardPostRequestDto.java
@@ -5,12 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class VacBoardPostRequestDto {
     private Long userId;
+
+    @NotBlank(message = "제목을 입력해주세요.")
     private String title;
+    @NotBlank(message = "내용을 적어주세요")
     private String contents;
 }
